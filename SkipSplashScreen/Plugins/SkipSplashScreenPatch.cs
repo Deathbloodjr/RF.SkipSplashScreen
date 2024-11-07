18bf9d02911223a9965e5e9331f8d74a0119ec98
Deathblood <101686464+Deathbloodjr@users.noreply.github.com>
2024-11-06 21:18:21 -0600

    Update SkipSplashScreenPatch.cs
    Now skips the ESRB screen, and skips most of the PEGI screen.
    This isn't perfect, but it's an improvement.

--- SkipSplashScreen/Plugins/SkipSplashScreenPatch.cs
+++ SkipSplashScreen/Plugins/SkipSplashScreenPatch.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine.Playables;
 
 namespace SkipSplashScreen.Plugins
 {
@@ -21,16 +22,54 @@ namespace SkipSplashScreen.Plugins
         [HarmonyPatch(nameof(BootSceneUiController.Method_Private_UniTask_Single_Boolean_0))]
         [HarmonyPatch(MethodType.Normal)]
         [HarmonyPrefix]
-        public static bool BootSceneUiController_StartAsync_Prefix(BootSceneUiController __instance, ref float duration)
+        public static bool BootSceneUiController_StartAsync_PlayAsync_Prefix(BootSceneUiController __instance, ref float duration)
         {
-            //Logger.Log("");
-            //Logger.Log("BootSceneUiController_StartAsync_Prefix");
+            Logger.Log("BootSceneUiController_StartAsync_PlayAsync_Prefix");
 
             SkipFade = true;
             duration = newDuration;
 
             return true;
         }
+
+        [HarmonyPatch(typeof(BootSceneUiController))]
+        [HarmonyPatch(nameof(BootSceneUiController.IsShowESRB))]
+        [HarmonyPatch(MethodType.Normal)]
+        [HarmonyPostfix]
+        public static void BootSceneUiController_IsShowESRB_Postfix(BootSceneUiController __instance, ref bool __result)
+        {
+            Logger.Log("BootSceneUiController_IsShowESRB_Postfix");
+
+            __result = false;
+        }
+
+        [HarmonyPatch(typeof(BootImage))]
+        [HarmonyPatch(nameof(BootImage.SetPEGI))]
+        [HarmonyPatch(MethodType.Normal)]
+        [HarmonyPrefix]
+        public static bool BootImage_SetPEGI_Prefix(BootImage __instance)
+        {
+            Logger.Log("BootImage_SetPEGI_Prefix");
+
+            __instance.Type.Value = BootImage.Types.AutoSaveWarning;
+
+            return false;
+        }
+
+        [HarmonyPatch(typeof(BootImage))]
+        [HarmonyPatch(nameof(BootImage.PlayAsync))]
+        [HarmonyPatch(MethodType.Normal)]
+        [HarmonyPrefix]
+        public static bool BootImage_PlayAsync_Prefix(BootImage __instance, ref float duration)
+        {
+            Logger.Log("BootImage_PlayAsync_Prefix");
+            SkipFade = true;
+            duration = newDuration;
+
+            return true;
+        }
+
+
 
         [HarmonyPatch(typeof(FadeCover))]
         [HarmonyPatch(nameof(FadeCover.FadeInAsync))]
